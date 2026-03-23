03c43b5b2b59bb2268bec429552fc83de055e2de
Roshani Mevada <roshanimevada124@gmail.com>
2026-03-23 11:13:48 +0530

    program to count frequency of elements in array

---
+++ counting_frequency.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 2, 3, 1};
+    int n = 5;
+
+    for(int i = 0; i < n; i++) {
+        int count = 1;
+
+        if(arr[i] == -1) continue;
+
+        for(int j = i + 1; j < n; j++) {
+            if(arr[i] == arr[j]) {
+                count++;
+                arr[j] = -1; // mark counted
+            }
+        }
+
+        cout << arr[i] << " -> " << count << endl;
+    }
+
+    return 0;
+}
