7b04d7a183ff4f62e69d552b73357084c61ba20d
Roshani Mevada <roshanimevada124@gmail.com>
2026-03-23 11:18:34 +0530

    Add program to reverse an array

---
+++ reverse_an_array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = 5;
+
+    int start = 0;
+    int end = n - 1;
+
+    while(start < end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+
+    // Print reversed array
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
